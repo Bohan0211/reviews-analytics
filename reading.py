94ad300b09b226350aa8553d23bb571e081c311c
Bohan Lu <bohan0211@gmail.com>
2022-04-03 20:35:33 +0800

    new file

---
+++ reading.py
@@ -0,0 +1,13 @@
+
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(data)
+
+print(data[3000])
+print('-------------------')
+print(data[4587])
