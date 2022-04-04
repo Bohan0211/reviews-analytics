67766f81c12e1bc2ea92e121563ea1a5e417d3f6
Bohan Lu <bohan0211@gmail.com>
2022-04-04 11:31:18 +0800

    third

--- reading.py
+++ reading.py
@@ -5,7 +5,7 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 10000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
 
 print('檔案讀取完了, 總共有', len(data), '筆資料')
@@ -13,3 +13,10 @@ total = 0
 for d in data:
 	total += len(d)
 print('留言的平均長度為:', total/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆資料長度小於100')
+print(new[0])
